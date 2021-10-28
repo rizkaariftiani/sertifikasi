0228b3aa287982509d64afdea5c7fc13db7087ef
rizkaariftiani <rizkaariftiani@gmail.com>
2021-10-28 14:35:01 +0700

    sertifikasi

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+
+@section('container')
+<div id="left" class='mt-3'>
+    <h1>About</h1>
+    <img src="img/rizka.jpg" alt="Rizka Nur Ariftiani" width="200" class="img-thumbnail rounded-circle float-left">
+</div>
+<div id="right">
+    <h5><b>Aplikasi Ini dibuat oleh</b></h5>
+    <h5>
+        <table>
+            <tr>
+                <td width="100px">Nama</td>
+                <td>: Rizka Nur Ariftiani</td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>: 1931710016</td>
+            </tr>
+            <tr>
+                <td>Tanggal</td>
+                <td>: 28 Oktober 2021</td>
+            </tr>
+        </table>
+    </h5>
+</div>
+
+@endsection
